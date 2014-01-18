c2b0d76cfc5f9415045314a7fd3a5a36ea770423
psmith <peter.smith@englandfencing.org.uk>
2014-01-18 22:45:28 +0000

    only add first tableau in a suite remove unused length variables

--- writexml.pl
+++ writexml.pl
@@ -187,16 +187,13 @@ sub do_comp
 	#	<xsl:variable name="rankingpagesize" select="number(30)"/>
 	#	<xsl:variable name="entrysize" select="number(138)"/>
 	#	<xsl:variable name="poolsperpage" select="number(2)"/>
-	#	<xsl:variable name="col1size" select="number(4)"/>
-	#	<xsl:variable name="tablistsize" select="number(8)"/>
+	
    
 	$out->{fpppagesize} = 30;
 	$out->{whereamipagesize} = 35;
 	$out->{rankingpagesize} = 30;
 	$out->{entrysize} = 138;
 	$out->{poolsperpage} = 2;
-	$out->{col1size} = 4;
-	$out->{tablistsize} = 8;
    
 	my $where = $c->whereami;
 	
